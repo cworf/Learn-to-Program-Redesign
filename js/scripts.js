1992cc87adfe67dc5bfe06bbb27596d3223eb088
cworf <colin.worf@gmail.com>
2017-12-08 16:29:49 -0800

    add style for all sizes of small grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
   $('ul.tabs').tabs('select_tab', 'tab_id');
   $('.parallax').parallax();
+  $(".button-collapse").sideNav();
+  // Add header class to body after 10px
+  $( window ).bind( 'load scroll', function() {
+
+      if ( $( window ).scrollTop() > 10 ) {
+
+          $( 'nav' ).addClass( 'scrolling' );
+
+      } else {
+
+          $( 'nav' ).removeClass( 'scrolling' );
+
+      }
+
+  });
+
 });
